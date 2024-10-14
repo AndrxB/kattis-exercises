c4ce4fd69ce6ffffafc00e845ecf9af7102d5757
André <arbi@itu.dk>
2024-10-14 22:04:37 +0200

    hw2

--- ITU/src/F2024/IDBS Exercises/HW2/HW2.sql
+++ ITU/src/F2024/IDBS Exercises/HW2/HW2.sql
@@ -2,21 +2,32 @@
 -- 1 ER Diagram Interpretation
 -- Choose the correct statements
 
--> (a) All foxes have a weight.
+(a) All foxes have a weight.
+
 -> (b) A hunter can kill a fox without the assistance of a dog.
+
 (c) A dog can kill a bird.
+
 (d) Hunters are animals.
+
 (e) All animals are dead.
+
 -> (f) A hunter can kill a fox with the assistance of at least one dog.
+
 -> (g) All foxes are dead.
+
 (h) A bird can be killed by many hunters.
+
 (i) All birds are dead.
--> (j) Given a hunter, it is possible to find the total weight of the animals he/she has killed.
+
+(j) Given a hunter, it is possible to find the total weight of the animals he/she has killed.
+
 -> (k) A bird can be a dog.
 */
 
 -- 2 ER Diagram Design (original by Hildur Davíðsdóttir)
 -- https://erdplus.com/edit-diagram/28cd0662-4f10-4ad0-935d-2e284a97db08
+
 
 -- 3 SQL
 
@@ -25,10 +36,10 @@
 empire ‘Iberian’ consist of?
 */
 
-SELECT COUNT(*) FROM empires WHERE empire LIKE 'Great%';
+SELECT COUNT(*) AS _3a_demo FROM empires WHERE empire LIKE 'Great%';
 -- 4
 
-SELECT COUNT(*) FROM empires WHERE empire LIKE 'Iberian%';
+SELECT COUNT(*) AS _3a FROM empires WHERE empire LIKE 'Iberian%';
 -- 3
 
 /*
@@ -36,7 +47,7 @@ SELECT COUNT(*) FROM empires WHERE empire LIKE 'Iberian%';
 these countries are partially in Asia?
 */
 
-SELECT COUNT(*) AS more_than_one_continent FROM (
+SELECT COUNT(*) AS _3b_demo FROM (
 	SELECT C.name
 	FROM countries C, countries_continents CC 
 	WHERE C.code = CC.countrycode 
@@ -45,7 +56,7 @@ SELECT COUNT(*) AS more_than_one_continent FROM (
 ) count;
 -- 4
 
-SELECT COUNT(*) AS partial_asia FROM (
+SELECT COUNT(*) AS _3b FROM (
 	SELECT C.name
 	FROM countries C, countries_continents CC 
 	WHERE C.code = CC.countrycode 
@@ -62,18 +73,45 @@ the database. How many people who speak Spanish exist in the countries of Europe
 that have more than 50 million inhabitants?
 */
 
+SELECT SUM(C.population * CL.percentage/100)AS _3c_demo
+FROM countries C, countries_continents CC, countries_languages CL 
+WHERE C.code = CC.countrycode 
+AND C.code = CL.countrycode 
+AND CC.continent LIKE 'North America' 
+AND C.population > 80000000 
+AND CL.language = 'Spanish';
+-- 111946176
+
+SELECT FLOOR(SUM(C.population * CL.percentage/100)) AS _3c
+FROM countries C, countries_continents CC, countries_languages CL 
+WHERE C.code = CC.countrycode 
+AND C.code = CL.countrycode 
+AND CC.continent LIKE 'Europe'
+AND C.population >  50000000
+AND CL.language = 'Spanish';
+-- 236902
 
 
 /*
 (d) According to the database, two languages are spoken in all countries of ‘Benelux’.
 How many languages are spoken in all countries of ‘Danish Empire’?
+-- Note: This is a division query; points will only be awarded if division is attempted.
 */
 
 
-
--- Note: This is a division query; points will only be awarded if division is attempted.
-
-
+/*
 -- 4 Functions & Triggers
 -- choose the correct statements
 
+(a) A row-level AFTER trigger on INSERT can modify the newly inserted row.
+no, the inserted record is already in the database, and will not the altered
+
+-> (b) In PostgreSQL, a trigger needs to be associated with a function.
+yes, you create a trigger based on a function.
+
+(c) A function can only be executed using a SELECT statement.
+no, you can create a function without a SELECT statement
+
+-> (d) If a function fails, its changes will be reverted.
+yes, and no? the changes will not be commited.
+*/
